2b5158263236dc23d966278ef608fcc6df96e465
M Bilal <mb1268223@gmail.com>
2026-01-11 15:58:35 +0500

    Increase JSON and URL-encoded body size limits

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.set("trust proxy", true);
 
